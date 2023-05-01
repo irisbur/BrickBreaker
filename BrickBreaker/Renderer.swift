d78841109ad1c34930e77144693de8aee8ae0349
Iris Burmistrov <iburmistrov@lightricks.com>
2023-05-01 18:29:59 +0300

    video4

--- BrickBreaker/Renderer.swift
+++ BrickBreaker/Renderer.swift
@@ -12,21 +12,29 @@ class Renderer: NSObject {
   let device: MTLDevice
   let commandQueue: MTLCommandQueue
 
-  var upperVertices: [Float] = [
-    -1, -1, 0,
-    1, 1, 0,
-    -1, 1, 0
-  ]
-
-  var lowerVertices: [Float] = [
+  var vertices: [Float] = [
+    -1, 1, 0,
     -1, -1, 0,
     1, -1, 0,
     1, 1, 0
   ]
 
+  var indices: [UInt16] = [
+  0, 1, 2,
+  2, 3, 0
+  ]
+
   var pipelineState: MTLRenderPipelineState?
-  var upperVertexBuffer: MTLBuffer?
-  var lowerVertexBuffer: MTLBuffer?
+  var vertexBuffer: MTLBuffer?
+  var indexBuffer: MTLBuffer?
+
+  struct Constants {
+    var animateBy: Float = 0.0
+  }
+
+  var constants = Constants()
+
+  var time: Float = 0
 
   init (device: MTLDevice) {
     self.device = device
@@ -37,10 +45,10 @@ class Renderer: NSObject {
   }
 
   private func buildModel() {
-    upperVertexBuffer = device.makeBuffer(bytes: upperVertices,
-                                     length: upperVertices.count * MemoryLayout<Float>.size, options: [])
-    lowerVertexBuffer = device.makeBuffer(bytes: lowerVertices,
-                                          length: lowerVertices.count * MemoryLayout<Float>.size, options: [])
+    vertexBuffer = device.makeBuffer(bytes: vertices,
+                                     length: vertices.count * MemoryLayout<Float>.size, options: [])
+    indexBuffer = device.makeBuffer(bytes: indices,
+                                    length: indices.count * MemoryLayout<UInt16>.size, options: [])
   }
 
   private func buildPipelineState() {
@@ -70,18 +78,26 @@ extension Renderer: MTKViewDelegate {
   func draw(in view: MTKView) {
     guard let drawable = view.currentDrawable,
           let pipelineState = pipelineState,
+          let indexBuffer = indexBuffer,
       let descriptor = view.currentRenderPassDescriptor else {
       return
     }
 
     let commandBuffer = commandQueue.makeCommandBuffer()!
+
+    time += 1 / Float(view.preferredFramesPerSecond)
+
+    let animateBy = abs(sin(time)/2+0.5)
+    constants.animateBy = animateBy
+
     let commandEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: descriptor)!
 
     commandEncoder.setRenderPipelineState(pipelineState)
-    commandEncoder.setVertexBuffer(upperVertexBuffer, offset: 0, index: 0)
-    commandEncoder.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: upperVertices.count)
-    commandEncoder.setVertexBuffer(lowerVertexBuffer, offset: 0, index: 0)
-    commandEncoder.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: upperVertices.count)
+    commandEncoder.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
+    commandEncoder.setVertexBytes(&constants, length: MemoryLayout<Constants>.stride, index: 1)
+    commandEncoder.drawIndexedPrimitives(type: .triangle, indexCount: indices.count,
+                                         indexType: .uint16, indexBuffer: indexBuffer,
+                                         indexBufferOffset: 0)
     commandEncoder.endEncoding()
     commandBuffer.present(drawable)
     commandBuffer.commit()
